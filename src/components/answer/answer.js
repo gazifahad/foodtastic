ec6696eb5d31b1540166cec980931b5171a56849
gazifahad <gazifahad3@gmail.com>
2022-03-26 22:43:33 +0600

    revisedn

--- src/components/answer/answer.js
+++ src/components/answer/answer.js
@@ -7,7 +7,7 @@ const Answer = () => {
             <p>
                 <h2>State vs props</h2>
                 
-                states are treated to be the heart of the components,It acts whenever something changes in a component, It is used for those components which are meant to be changed over time,Whenever the value of a certain component changes the state also changes, so state is updatable.
+                States are treated to be the heart of the components,It acts whenever something changes in a component, It is used for those components which are meant to be changed over time,Whenever the value of a certain component changes the state also changes, so state is updatable.
                 On the other hand props are objects which is immutabe,It only can be read,it carries the data from a parent component to it's child components.By passing data from one element to another prop makes components reusable.
             </p>
             <p>
